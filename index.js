5a8f945428beffcae02564c140ba238af669aea9
Madina <madinatussupova0820@gmail.com>
2022-08-28 10:34:45 +0600

    drum plays on click

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const drumBtns = document.querySelectorAll('.drum');
 
-drumBtns.forEach(btn => btn.addEventListener ('click', function() {
-	alert("I got clicked!");
-}));
+for(let i = 0; i < drumBtns.length; i++) {
+	drumBtns[i].addEventListener('click',  function() {
+		let btnInnerHTML = this.textContent;
 
-function add(num1, num2) {
-	return num1 + num2;
-}
+			switch (btnInnerHTML) {
+			case "w":
+				const drumSoundW = new Audio('sounds/tom-1.mp3');
+				drumSoundW.play();
+				break;
 
-function multiply(num1, num2) {
-	return num1 * num2;
-}
+			case "a":
+				const drumSoundA = new Audio('sounds/tom-2.mp3');
+				drumSoundA.play();
+				break;
 
-function subtract(num1, num2) {
-	return num1 - num2;
-}
+				case "s":
+					const drumSoundS = new Audio('sounds/tom-3.mp3');
+					drumSoundS.play();
+					break;
 
-function divide(num1, num2) {
-	return num1 / num2;
-}
+				case "d":
+					const drumSoundD = new Audio('sounds/tom-4.mp3');
+					drumSoundD.play();
+					break;
 
-function calculator(num1, num2, operator) {
-	return operator(num1, num2)
+				case "j":
+					const drumSoundJ = new Audio('sounds/snare.mp3');
+					drumSoundJ.play();
+					break;
+
+				case "k":
+					const drumSoundK = new Audio('sounds/crash.mp3');
+					drumSoundK.play();
+					break;
+
+				case "l":
+					const drumSoundL = new Audio('sounds/kick-bass.mp3');
+					drumSoundL.play();
+					break;
+				
+			default: console.log(btnInnerHTML);
+				break;
+		}
+	});
 };
-
-console.log(calculator(6, 3, divide));
-
-
-// for(let i = 0; i < drumBtns.length; i++) {
-// 	drumBtns[i].addEventListener('click',  function() {
-// 	})
-// };
